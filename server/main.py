21b5359b9dec5460998400ba46df5504607a5ddc
warren yun <xxwarrenhdxx@gmail.com>
2021-04-17 09:33:47 -0400

    made createUser method

--- server/main.py
+++ server/main.py
@@ -1,7 +1,6 @@
 import graphene, json
 import pymysql
 import secrets
-import graphql_query
 
 from flask_sqlalchemy import SQLAlchemy 
 from flask import Flask, request
@@ -13,28 +12,62 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = conn
 db = SQLAlchemy(app) 
 
+
 # model of user table from SQL database
-class DB_User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.VARCHAR(255), unique=True, nullable=False)
     display_name = db.Column(db.VARCHAR(25), unique=True, nullable=False)
     coin_val = db.Column(db.Integer)
-
     # representation of data for testing
     def __repr__(self):
-        return "id: {0} | email : {1}".format(self.id, self.email)
+        return "successfully added user {}, named {}".format(id, display_name)
 
+class Query(graphene.ObjectType):
+    createUser = graphene.String(id=graphene.ID(), email=graphene.String(), displayName=graphene.String(), coinVal=graphene.Int())
+    data = graphene.String(name=graphene.String(), age=graphene.Int())
+    
+    """
+    createUser method
 
-# structure of a query to graphql API
+    Fields
+    ------
+    id : int
+        Unique User ID number
+    email : str
+        User's email
+    displayName : str
+        The display name for the user
+    coinVal : int
+        The number of "CoinCoins" the user has
 
-schema = graphene.Schema(query=graphql_query.Query)
+    """
+    def resolve_createUser(root, info, id, email, displayName, coinVal):
+        newUser_json = {
+            "id": id,
+            "email": email,
+            "displayName": displayName,
+            "coinVal": coinVal
+        }
+        newUser = Users(id=id, email=email, display_name=displayName, coin_val=coinVal)
+        db.session.add(newUser)
+        db.session.commit()
+        return newUser_json
 
-@app.route("/graphql", methods=["POST"])
+    def resolve_data(root, info, name, age):
+        return name, age
+        
+
+schema = graphene.Schema(query=Query)
+
+@app.route("/graphql", methods=["GET", "POST"])
 def graphql():
     data = json.loads(request.data)
     result = schema.execute(data['query'])
-    #items = dict(result.data.items())
-    return json.dumps(result.data)
+    if request.method == "GET":
+        return "yes"
+    if request.method == "POST":
+        return json.dumps(result.data)
 
 if __name__ == '__main__':
     app.run(debug=True)
